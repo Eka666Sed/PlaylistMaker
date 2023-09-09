987c3b306f7a904f8d9518b8e3148ed1ead1f2c9
Eka666Sed <mrs_sedova@mail.ru>
2023-09-09 19:40:30 +0200

    Commit 11 after fix

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -18,108 +18,103 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.databinding.ActivitySearchBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class SearchActivity : AppCompatActivity() {
-    private lateinit var toolbarSettings: Toolbar
-    private lateinit var search: EditText
-    private lateinit var button: ImageButton
-    private lateinit var rv: RecyclerView
-    private lateinit var iVMessage: ImageView
-    private lateinit var tvMessage: TextView
-    private lateinit var btnMessage: Button
+    private var toolbarSettings: Toolbar? = null
+    private var search: EditText? = null
+    private var button: ImageButton? = null
+    private var rv: RecyclerView? = null
+    private var iVMessage: ImageView? = null
+    private var tvMessage: TextView? = null
+    private var btnMessage: Button? = null
     private var savedText: String = ""
-
-    companion object {
-        const val search_text = "search_text"
-        const val noWeb = "Проблемы со связью \n\nЗагрузка не удалась. " +
-                "Проверьте подключение к интернету"
-        const val noContent = "Ничего не нашлось"
-    }
+    private var binding:ActivitySearchBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
-        search = findViewById(R.id.editTextSearch)
-        button = findViewById(R.id.clear_button)
-        rv = findViewById(R.id.track_recycler_view)
-        iVMessage = findViewById(R.id.iv_message)
-        tvMessage = findViewById(R.id.tv_message)
-        btnMessage = findViewById(R.id.btn_message)
-        toolbarSettings = findViewById(R.id.toolbarSettings)
-        button.visibility = View.INVISIBLE
-        toolbarSettings.setNavigationOnClickListener { onBackPressed() }
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        search = binding?.editTextSearch
+        button = binding?.clearButton
+        rv = binding?.trackRecyclerView
+        iVMessage = binding?.ivMessage
+        tvMessage = binding?.tvMessage
+        btnMessage = binding?.btnMessage
+        toolbarSettings = binding?.toolbarSettings
+        button?.visibility = View.INVISIBLE
+        toolbarSettings?.setNavigationOnClickListener { onBackPressed() }
         searchButton()
         updateData()
         if (savedInstanceState != null) {
-            savedText = savedInstanceState.getString((search_text), "")
-            search.setText(savedText)
+            savedText = savedInstanceState.getString((getString(R.string.search_text)), "")
+            search?.setText(savedText)
         }
         textWatcher()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        savedText = search.text.toString()
-        outState.putString((search_text), savedText)
+        savedText = search?.text.toString()
+        outState.putString((getString(R.string.search_text)), savedText)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        savedText = savedInstanceState.getString((search_text), "")
-        search.setText(savedText)
+        savedText = savedInstanceState.getString((getString(R.string.search_text)), "")
+        search?.setText(savedText)
     }
 
     private fun textWatcher() {
         val myTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                button.visibility = View.INVISIBLE
+                button?.visibility = View.INVISIBLE
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (s.isNullOrEmpty()) {
-                    button.visibility = View.INVISIBLE
+                    button?.visibility = View.INVISIBLE
                 } else {
-                    button.visibility = View.VISIBLE
+                    button?.visibility = View.VISIBLE
                 }
             }
 
             override fun afterTextChanged(s: Editable?) {}
         }
-        search.doOnTextChanged { text, _, _, _ ->
+        search?.doOnTextChanged { text, _, _, _ ->
             myTextWatcher.onTextChanged(text, 0, 0, 0)
         }
-        button.setOnClickListener {
-            search.text.clear()
+        button?.setOnClickListener {
+            search?.text?.clear()
             hideKeyboard()
             hidePicture()
             clearAdapter()
         }
     }
-
     private fun hidePicture(){
-        iVMessage.visibility = View.INVISIBLE
-        tvMessage.visibility = View.INVISIBLE
+        iVMessage?.visibility = View.INVISIBLE
+        tvMessage?.visibility = View.INVISIBLE
     }
 
     private fun hideKeyboard() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(button.windowToken, 0)
+        imm.hideSoftInputFromWindow(button?.windowToken, 0)
     }
 
     private fun clearAdapter(){
         val clearAdapter = TrackAdapter(this)
         clearAdapter.clearListAdapter()
-        rv.adapter = clearAdapter
+        rv?.adapter = clearAdapter
     }
     private fun updateData(){
-        btnMessage.setOnClickListener {getWebRequest()}
+        btnMessage?.setOnClickListener {getWebRequest()}
     }
 
     private fun searchButton() {
-        search.setOnEditorActionListener { _, actionId, _ ->
+        search?.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 clearAdapter()
                 getWebRequest()
@@ -128,7 +123,7 @@ class SearchActivity : AppCompatActivity() {
         }
     }
     private fun getWebRequest(){
-        val query = search.text.toString().trim()
+        val query = search?.text.toString().trim()
         val apiService = TrackApiService.create
         apiService.search(query).enqueue(object : Callback<ResponseTrack> {
             override fun onResponse(
@@ -149,31 +144,31 @@ class SearchActivity : AppCompatActivity() {
         val trackList = response.body()?.results ?: emptyList()
 
         if (response.isSuccessful && trackList.isNotEmpty()) {
-            rv.visibility = View.VISIBLE
-            iVMessage.visibility = View.INVISIBLE
-            btnMessage.visibility = View.INVISIBLE
-            tvMessage.text = ""
+            rv?.visibility = View.VISIBLE
+            iVMessage?.visibility = View.INVISIBLE
+            btnMessage?.visibility = View.INVISIBLE
+            tvMessage?.text = ""
 
             val trackAdapter = TrackAdapter(this@SearchActivity)
-            rv.layoutManager = LinearLayoutManager(this@SearchActivity)
-            rv.adapter = trackAdapter
+            rv?.layoutManager = LinearLayoutManager(this@SearchActivity)
+            rv?.adapter = trackAdapter
             trackAdapter.updateData(trackList)
 
         } else {
-            rv.visibility = View.INVISIBLE
-            iVMessage.visibility = View.VISIBLE
-            tvMessage.visibility = View.VISIBLE
-            btnMessage.visibility = View.INVISIBLE
+            rv?.visibility = View.INVISIBLE
+            iVMessage?.visibility = View.VISIBLE
+            tvMessage?.visibility = View.VISIBLE
+            btnMessage?.visibility = View.INVISIBLE
 
             if (response.isSuccessful) {
-                tvMessage.text = noContent
-                iVMessage.setImageResource(
+                tvMessage?.text = getString(R.string.no_content)
+                iVMessage?.setImageResource(
                     if (isNightModeEnabled()) R.drawable.dark_no_content
                     else R.drawable.no_content
                 )
             } else {
-                tvMessage.text = noWeb
-                iVMessage.setImageResource(
+                tvMessage?.text = getString(R.string.no_web)
+                iVMessage?.setImageResource(
                     if (isNightModeEnabled()) R.drawable.dark_no_content
                     else R.drawable.no_content
                 )
@@ -182,12 +177,12 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun handleFailure() {
-        rv.visibility = View.INVISIBLE
-        iVMessage.visibility = View.VISIBLE
-        tvMessage.visibility = View.VISIBLE
-        btnMessage.visibility = View.VISIBLE
-        tvMessage.text = noWeb
-        iVMessage.setImageResource(
+        rv?.visibility = View.INVISIBLE
+        iVMessage?.visibility = View.VISIBLE
+        tvMessage?.visibility = View.VISIBLE
+        btnMessage?.visibility = View.VISIBLE
+        tvMessage?.text = getString(R.string.no_web)
+        iVMessage?.setImageResource(
             if (isNightModeEnabled()) R.drawable.dark_no_web
             else R.drawable.no_web
         )
@@ -197,7 +192,4 @@ class SearchActivity : AppCompatActivity() {
         val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
         return currentNightMode == Configuration.UI_MODE_NIGHT_YES
     }
-
-
-
 }
--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,56 +1,20 @@
 package com.example.playlistmaker
 
 import android.content.Context
-import android.util.TypedValue
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 
 
 class TrackAdapter(private val context: Context) :
-    RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
+    RecyclerView.Adapter<TrackViewHolder>() {
 
     private var listTrack: List<Track> = emptyList()
-
-   inner class TrackViewHolder(track: View) : RecyclerView.ViewHolder(track) {
-       private val artistName: TextView = itemView.findViewById(R.id.tvArtistName)
-       private val trackName: TextView = itemView.findViewById(R.id.tvTrackName)
-       private val trackTime: TextView = itemView.findViewById(R.id.tvTrackTime)
-       private val artworkUrl100: ImageView = itemView.findViewById(R.id.artworkUrl100)
-
-        fun bind(track: Track) {
-            val requestOptions = RequestOptions()
-                    .placeholder(R.drawable.placeholder)
-                    .diskCacheStrategy(DiskCacheStrategy.ALL)
-
-            artistName.text = track.artistName
-            trackName.text = track.trackName
-            trackTime.text = track.getFormattedTrackTime()
-            Glide.with(itemView)
-                .applyDefaultRequestOptions(requestOptions)
-                .load(track.artworkUrl100)
-                .transform(CenterCrop(), RoundedCorners(dpToPx(radiusCorners,context)))
-                .into(artworkUrl100)
-        }
-       private fun dpToPx(dp: Float, context: Context): Int {
-           return TypedValue.applyDimension(
-               TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics
-           ).toInt()
-       }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_track_list, parent, false)
-        return TrackViewHolder(view)
+        return TrackViewHolder(view, context)
     }
 
     override fun getItemCount(): Int = listTrack.size
@@ -60,16 +24,13 @@ class TrackAdapter(private val context: Context) :
         holder.bind(item)
     }
 
-     fun updateData(newListTrack: List<Track>) {
+    fun updateData(newListTrack: List<Track>) {
         listTrack = newListTrack
         notifyDataSetChanged()
     }
-     fun clearListAdapter(){
+
+    fun clearListAdapter() {
         listTrack = emptyList()
         notifyDataSetChanged()
-    }
-
-     companion object{
-        val radiusCorners = 2.0f
     }
 }
