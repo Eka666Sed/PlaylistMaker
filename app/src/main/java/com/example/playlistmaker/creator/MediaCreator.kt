856defadf8c99754c8cc2fe13855b91cc8cef14e
Eka666Sed <mrs_sedova@mail.ru>
2023-12-21 00:24:15 +0100

    Sprint 16 for check after fix rm files

--- app/src/main/java/com/example/playlistmaker/creator/MediaCreator.kt
+++
@@ -1,8 +0,0 @@
-package com.example.playlistmaker.creator
-
-import com.example.playlistmaker.domain.player.PlayerInteractor
-import com.example.playlistmaker.domain.player.PlayerInteractorlmpl
-
-object MediaCreator {
-    fun providePlayerInteractor() : PlayerInteractor = PlayerInteractorlmpl()
-}
--- app/src/main/java/com/example/playlistmaker/creator/StorageCreator.kt
+++
@@ -1,9 +0,0 @@
-package com.example.playlistmaker.creator
-
-import com.example.playlistmaker.data.setting.impl.StorageRepositoryImpl
-import com.example.playlistmaker.data.setting.impl.StorageInteractorImpl
-import com.example.playlistmaker.domain.settings.model.StorageInteractor
-
-object StorageCreator {
-    fun provideStorageInteractor() : StorageInteractor = StorageInteractorImpl(StorageRepositoryImpl())
-}
